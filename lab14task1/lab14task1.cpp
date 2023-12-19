40772b189cd19d831fdcc1a5d2527064a5369b5c
Anisiia Yalyzhko <anisiya.sumy.ua@gmail.com>
2023-12-20 00:13:14 +0200

    test commit

--- lab14task1/lab14task1.cpp
+++ lab14task1/lab14task1.cpp
@@ -1,20 +1,70 @@
 ﻿// lab14task1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main()
-{
-    int a, b, c;
+void findMinMaxAndDistance(int* arr, int size, int* min, int* max, int* distance) {
+    // Ініціалізуємо min та max початковими значеннями
+    *min = *arr;
+    *max = *arr;
+
+    // Знаходимо min та max в масиві
+    for (int i = 1; i < size; i++) {
+        if (*(arr + i) < *min) {
+            *min = *(arr + i);
+        }
+        if (*(arr + i) > *max) {
+            *max = *(arr + i);
+        }
+    }
+
+    // Знаходимо відстань між min та max
+    int minIndex = -1, maxIndex = -1;
+    for (int i = 0; i < size; i++) {
+        if (*(arr + i) == *min) {
+            minIndex = i;
+        }
+        if (*(arr + i) == *max) {
+            maxIndex = i;
+        }
+    }
+
+    // Обчислюємо відстань
+    *distance = abs(maxIndex - minIndex);
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+int main() {
+    int size;
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+    // Ввід розміру масиву
+    printf("Enter the massive: ");
+    scanf_s("%d", &size);
+
+    // Виділення пам'яті для масиву
+    int* arr = (int*)malloc(size * sizeof(int));
+
+    // Ввід елементів масиву
+    printf("Enter the elements of the massive:\n");
+    for (int i = 0; i < size; i++) {
+        scanf_s("%d", arr + i);
+    }
+
+    int min, max, distance;
+
+    // Знаходження min, max та відстані
+    findMinMaxAndDistance(arr, size, &min, &max, &distance);
+
+    // Виведення результатів
+    printf("min: %d\n", min);
+    printf("max: %d\n", max);
+    printf("distance: %d\n", distance);
+
+    // Звільнення виділеної пам'яті
+    free(arr);
+
+    return 0;
+}
+
+
+
